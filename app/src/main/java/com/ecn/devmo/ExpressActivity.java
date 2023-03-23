f7d45fce6e3cad0375fbf2e76562058b6eab7dc4
Zilong WU <95653923+littlewuuu@users.noreply.github.com>
2023-03-23 08:57:30 +0100

    express items page

--- app/src/main/java/com/ecn/devmo/ExpressActivity.java
+++ app/src/main/java/com/ecn/devmo/ExpressActivity.java
@@ -1,13 +1,14 @@
 package com.ecn.devmo;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ExpressActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_express);
     }
 }
