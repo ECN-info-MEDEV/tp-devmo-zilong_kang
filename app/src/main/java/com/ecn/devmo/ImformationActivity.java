c913c720120a95ae4fb106f0d3d8839949a1b3a6
Zilong WU <95653923+littlewuuu@users.noreply.github.com>
2023-03-23 11:36:22 +0100

    information page

---
+++ app/src/main/java/com/ecn/devmo/ImformationActivity.java
@@ -0,0 +1,14 @@
+package com.ecn.devmo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imformation);
+    }
+}
