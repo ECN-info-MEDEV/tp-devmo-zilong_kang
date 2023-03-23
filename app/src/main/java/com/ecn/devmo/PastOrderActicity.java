1c5eca7b0b146476691ba305bfaebcb1f3971020
Zilong WU <95653923+littlewuuu@users.noreply.github.com>
2023-03-23 10:57:32 +0100

    past orders page

---
+++ app/src/main/java/com/ecn/devmo/PastOrderActicity.java
@@ -0,0 +1,14 @@
+package com.ecn.devmo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PastOrderActicity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_past_order_acticity);
+    }
+}
